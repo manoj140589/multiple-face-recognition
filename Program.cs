aa5b7710649a50fbb991c7dccba1c357ce3ce931
Manoj Vishwakarma <manoj.vishwakarma@hp.co.in>
2020-02-18 08:33:45 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MultiFaceRec
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new FrmPrincipal());
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Startup Issue");
+            }
+        }
+    }
+}
